32a47b7d828d4f4276e57a81155d3ac150273423
Rahul Raj <rahulmehtashr@gmail.com>
2024-08-06 03:15:11 +0530

    Added NotFound Page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './Pages/Home'
 import { Route, Routes } from 'react-router-dom'
 import Signup from './Pages/Auth/Signup'
 import Login from './Pages/Auth/Login'
+import NotFound from './Pages/NotFound'
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
       <Route path='/' element ={<Home />} />
       <Route path='/auth/signup' element ={<Signup />} />
       <Route path='/login' element ={<Login />} />
+      <Route path='*' element ={NotFound} />
     </Routes>
     
    </>
