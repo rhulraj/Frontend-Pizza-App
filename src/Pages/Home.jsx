7758aee979d79a7f75fe29ae97b736ae9923c173
Rahul Raj <rahulmehtashr@gmail.com>
2024-08-05 18:53:22 +0530

    Added pointer about App

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,9 @@
-import IconArrowRight from "../Components/Icons/ArrowRight"
+import IconArrowRight from "../Components/Icons/ArrowRight";
 
-import PizzaImage from "../assets/Images/pizza1.png"
+import PizzaImage from "../assets/Images/pizza1.png";
+import cookingImage from "../assets/Images/Cook1.png"
+import IconPatchCheck from "../Components/Icons/IconPatchCheck";
+
 function Home(){
     return (
         <div>
@@ -14,7 +17,7 @@ function Home(){
                                 Enjoy the Slice {' '}
                             </h1>
                             <h1>
-                                🤤
+                                😋
                             </h1>
 
                          </div>
@@ -36,6 +39,59 @@ function Home(){
                         width={550}
                         height={550} />
                     </div>
+
+            </section>
+
+
+            {/* Services Section */}
+
+            <section 
+                className="py-4 mt-6 bg-gradient-to-r from-amber-50 to-orange-300"
+            >
+                <div className="container flex flex-col md:flex-row">
+                    <div className="flex flex-col item-center justify-center rounded-lg lg:w-1/2">
+                        <img src={cookingImage} 
+                        alt="cooking"
+                        width={500}
+                        className="rounded-lg ml-8" />
+
+                    </div>
+                    <div className="flex flex-col flex-wrap text-center lg:py-6 lg:w-1/2 lg:pl-12 lg:text-left">
+                       <div>
+                            <div>
+                                <h2 className="mb-2 text-5xl font-extrabold text-transparent title-font bg-gradient-to-r from-orange-600 to-orange-300 bg-clip-text">
+                                    Cooked by the best <br /> chefs in the world
+                                </h2>
+                                <p className="text-base leading-releaxed text-[#6B7280]">
+                                    There are many benifitx regarding to that but the main ones are
+                                </p>
+                            </div>
+                       </div>
+                       <div className="w-full p-1">
+                           <div className="flex items-center h-full p-2 text-2xl rounded">
+                            <IconPatchCheck className ="text-[#F38339] w-10 h-10 mr-4" />
+                              <span className="font-bold title-font">Perfect Taste</span>
+                           </div>
+
+                       </div>
+                       <div className="w-full p-1">
+                           <div className="flex items-center h-full p-2 text-2xl rounded">
+                            <IconPatchCheck className ="text-[#F38339] w-10 h-10 mr-4" />
+                              <span className="font-bold title-font">Prepared quickly</span>
+                           </div>
+
+                       </div>
+                       <div className="w-full p-1">
+                           <div className="flex items-center h-full p-2 text-2xl rounded">
+                            <IconPatchCheck className ="text-[#F38339] w-10 h-10 mr-4" />
+                              <span className="font-bold title-font">Food hygene guarenteed</span>
+                           </div>
+
+                       </div>
+                    </div>
+
+                </div>
+
 
             </section>
         </div>
