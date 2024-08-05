3be42c67a352bbda96e3463b9d2db50bf88567c2
Rahul Raj <rahulmehtashr@gmail.com>
2024-08-05 14:45:22 +0530

    Added Buy Now Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './Pages/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-    home
-    </>
+   <div className='text-yellow-800'>
+    <Home />
+   </div>
   )
 }
 
